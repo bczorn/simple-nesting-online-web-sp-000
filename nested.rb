36da9c7ee96e6a855dbc3b699ca1374530f911e5
Becky Zorn <bczorn@gmail.com>
2019-09-11 04:39:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
+x = programmer_hash[:dennis_ritchie][:languages]
+return x
 end
 
 def adding_matz
